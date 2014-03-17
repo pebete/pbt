98f29e242c1745d4924d35a6572917344b97878d
jairot <jairotrad@gmail.com>
2014-03-16 21:58:59 -0300

    created the new first version for the new plugin

--- src/pbt_util.py
+++ src/pbt_util.py
@@ -1,5 +1,7 @@
 """utilities for pbt"""
 import os
+import subprocess
+import shlex
 
 def get_dirs_up_to_root(basepath):
     """return a list of all the paths that are directories from path up to root"""
@@ -15,3 +17,10 @@ def get_dirs_up_to_root(basepath):
         dirname, _ = os.path.split(dirname)
 
     return result
+
+def install_package(name_or_link):
+    # TODO: Ask if we are in a a virtualenv
+    subprocess.call(shlex.split("sudo pip3 install %s" % name_or_link))
+
+
+
