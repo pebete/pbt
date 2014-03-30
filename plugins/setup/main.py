3a6b898b7f7e4bddf0a4e76371b73befaff55379
Marcos Dione <mdione@grulic.org.ar>
2014-03-30 14:34:29 +0200

    [+] initial version of the setup plugin. has no support for packages, scripts (yet) or classifiers.

---
+++ plugins/setup/main.py
@@ -0,0 +1,40 @@
+"""The setup plugin: when the meta-problem counter reaches a high level"""
+import pbt
+import os
+
+def writeln(file, line=""):
+    "Pascal's turning in its grave."
+    file.write(line)
+    file.write(os.linesep)
+
+@pbt.command(name="setup")
+def main(ctx, args, p):
+    """Generates a valid setup.py file.
+    It uses the info found in project.pbt."""
+
+    # NOTE: yes, short names, 'cause I'm gonna write them a gazillion times
+    f = open('setup.py', 'w+')
+    s = p.settings
+
+    # TODO: support other major versions
+    writeln(f, "#! /usr/bin/env python3")
+    writeln(f, "from distutils.core import setup")
+    writeln(f)
+    writeln(f, "setup(")
+
+    # lazy, me?
+    def kv (k, v):
+        writeln(f, "    %s = '%s'," % (k, v))
+
+    kv('name', p.name)
+    kv('version', p.version)
+    kv('description', p.description)
+    # TODO: author
+    kv('url', p.url)
+    kv('license', p.license['name'])
+    # TODO: classifiers
+
+    writeln(f, "    )")
+    f.close()
+
+    print("setup.py file written.")
