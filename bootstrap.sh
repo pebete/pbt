4efe4454e7779d0fe3961057e4d9192d2dc07537
Marcos Dione <mdione@grulic.org.ar>
2014-04-09 13:29:39 +0200

    [*] in fact, we should also put this source dir before the rest, so also the tests use this pbt package and not one found somewhere else in the PYTHONPATH.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 pwd=$(pwd)
 # so import pbt works
-export PYTHONPATH="$PYTHONPATH:$pwd"
+export PYTHONPATH="$pwd:$PYTHONPATH"
 # so it finds only the plugins from the source
 export PBT_PLUGINS_PATH="$pwd/plugins"
 
