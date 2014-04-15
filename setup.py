202c3a5af2a0c578f827c52ca5a97c8edded67df
Marcos Dione <mdione@grulic.org.ar>
2014-04-15 21:38:41 +0200

    [+] we don't bootstrap yet, so this is needed. [+] install plugins in the user's xdg data dir, except helloworld.

---
+++ setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+from os.path import join
+from xdg.BaseDirectory import save_data_path, load_data_paths
+
+# plugins
+# according to the Debian Python Policy, they should go in /usr/share/<program>
+# NOTE: for the time being, we're gonna install them in the user's xdg.data dir
+
+# first, make sure the dir exists
+save_data_path("pbt")
+
+# the user's dir comes first
+user_xdg_data_dir = next(load_data_paths('pbt/plugins'))
+
+setup(
+    name='pbt',
+    version='0.0.1',
+    description='python build tool',
+    url='https://github.com/pebete/pbt',
+    packages=['pbt'],
+    scripts=['bin/pbt'],
+    license='Apache 2.0',
+    data_files=[
+        (join(user_xdg_data_dir, 'install'), ['plugins/install/main.py',]),
+        (join(user_xdg_data_dir, 'run'),     ['plugins/run/main.py',]),
+        (join(user_xdg_data_dir, 'new'),     ['plugins/new/main.py',
+                                              'plugins/new/templates.json',]),
+        (join(user_xdg_data_dir, 'check'),   ['plugins/check/main.py',]),
+        (join(user_xdg_data_dir, 'deb'),     ['plugins/deb/main.py',]),
+        (join(user_xdg_data_dir, 'setup'),   ['plugins/setup/main.py']),
+        ],
+    )
