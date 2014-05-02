28fca9b9e01dc49c94bf56e9a1569e5ba027d8a2
gilgamezh <mail@gilgamezh.me>
2014-05-02 14:57:03 -0300

    No more venvs here!!

--- plugins/try/main.py
+++ plugins/try/main.py
@@ -4,9 +4,6 @@ import tempfile
 import sys
 import pip
 from IPython import embed
-from venv import create
-from os.path import join
-
 
 
 @pbt.command(runs_in_project=False,name='try')
@@ -14,7 +11,7 @@ def main(ctx, args):
     """opens a shell with a library available to try it
 
 
-    pbt try [lib_to_try]        - create a disposable venv and install the passed lib, then give you
+    pbt try [lib_to_try]        - Install the passed lib in a disposable path, then give you
                                   a iPython terminal to try it.
 
 
@@ -25,26 +22,20 @@ def main(ctx, args):
         print('Usage: pbt try [lib_to_try]\n')
 
 
-
 def pbt_try(lib_to_try):
-    banner1='Testing {} in a disposable venv'.format(lib_to_try)
-    exit_msg= 'Do you like de lib? You can install it with pbt install ;)'
-    with tempfile.TemporaryDirectory() as tmp_venv_dir:
-        #Creating venv
-        create(tmp_venv_dir)
-        #activating venv
-        sys.base_prefix = sys.prefix
-        sys.base_exec_prefix = sys.exec_prefix
-        sys.prefix = tmp_venv_dir
-        sys.exec_prefix = tmp_venv_dir
-        lib_path = join(tmp_venv_dir,
-                       'lib',
-                       'python{}.{}'.format(*sys.version_info[:2]),
-                       'site-packages')
-        sys.path.insert(0,lib_path)
-        #Installing lib to try
-        pip_args = ['-v','install',"-t", lib_path,lib_to_try]
+    banner1='''{0} is installed in a disposable path and ready to test.
+    Maybe you should run "import {0}"'''.format(lib_to_try)
+    exit_msg= '''Do you like this lib? You can add it to your
+    project with pbt install ;)'''
+    # Installing
+    pbt_try_install(lib_to_try)
+    # Running iPython
+    embed(banner1=banner1, exit_msg=exit_msg)
+
+
+def pbt_try_install(lib_to_try):
+    with tempfile.TemporaryDirectory() as tmp_pbt_try_dir:
+        sys.path.insert(0,tmp_pbt_try_dir)
+        pip_args = ['-v','install',"-t", tmp_pbt_try_dir, lib_to_try]
         pip.main(initial_args=pip_args)
-        # Running iPython
-        embed(banner1=banner1, exit_msg=exit_msg)
 
---
+++ plugins/try/test_try.py
@@ -0,0 +1,12 @@
+"Test for try plugin"
+import pbt
+import unittest
+
+class PbtTryTestCase(unittest.TestCase):
+
+    def test_pbt_try_install(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
