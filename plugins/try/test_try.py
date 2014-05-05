515e538c019cd2adeb77d1000a5dc4cf8d8974e1
jairot <jairotrad@gmail.com>
2014-05-04 23:38:04 -0300

    test for try plugin

--- plugins/try/test_try.py
+++ plugins/try/test_try.py
@@ -2,11 +2,49 @@
 import pbt
 import unittest
 
-class PbtTryTestCase(unittest.TestCase):
+from unittest import mock
 
-    def test_pbt_try_install(self):
+gctx = pbt.global_ctx
+gctx.initial_setup()
+
+class fakepip(object):
+    def main():
         pass
-    #TODO :$
+
+class fakecode():
+    def interact():
+        pass
+
+class fakeipython():
+    def embed():
+        pass
+
+class TryTestCase(unittest.TestCase):
+
+    def test_try_is_loaded(self):
+        self.assertTrue(gctx.is_command("try"))
+
+    def test_try_noipython(self):
+        fakepip.main = mock.MagicMock()
+        fakecode.interact= mock.MagicMock()
+        with mock.patch.dict('sys.modules', {"pip": fakepip, "IPython": None,
+                                             "code": fakecode}):
+            gctx.run("try", ["requests"])
+        # Can't Check on the call args because of the random folder
+        self.assertTrue(fakepip.main.call_count==1)
+        self.assertTrue(fakecode.interact.call_count==1)
+
+    def test_try_ipython(self):
+        fakepip.main = mock.MagicMock()
+        fakecode.interact= mock.MagicMock()
+        fakeipython.embed =  mock.MagicMock()
+        with mock.patch.dict('sys.modules', {"pip": fakepip, "IPython": fakeipython,
+                                             "code": fakecode}):
+            gctx.run("try", ["requests"])
+        # Can't Check on the call args because of the random folder
+        self.assertTrue(fakepip.main.call_count==1)
+        self.assertTrue(fakecode.interact.call_count==0)
+        self.assertTrue(fakeipython.embed.call_count==1)
 
 
 if __name__ == "__main__":
